48c24e79f22cacd1f4cc887d18b4d432039aec4e
Nafiz Rahman <100969321+nafizR@users.noreply.github.com>
2026-08-15 13:54:15 -0500

    Added GameManager
    Made a script and moved some scripts no real work just setup.

---
+++ Scripts/Game_Manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+###Global Signals
+##Outgoing Signals
+#Round Start
+#Round End
+##Incoming Signals
+#Chip Landed
+
+##Global Variables
+#Current Round
+#Current Score
+
+##Global Functions
+#Start Round
+#End Round
